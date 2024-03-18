fa76ba2cc5b360907e13f8bfd4463c3f1a94a0fd
KUCINGABU234 <162587020+KUCINGABU234@users.noreply.github.com>
2024-03-18 22:30:14 +0700

    Update Start.py

--- Start.py
+++ Start.py
@@ -1,47 +1,45 @@
-from tqdm 
-import tqdm
-import time 
 import os
 import time
 import random
-
-for i in tqdm(range(10), desc="Loading", bar_format="{l_bar}{bar} {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}{postfix}]"):
-    time.sleep(0.5) 
+from tqdm import tqdm
 
 def clear_screen():
     os.system('clear')
 
 def display_intro():
     clear_screen()
-    print("welcome to my simple game")
+    print("Welcome to my simple game")
     input("Press enter to start the game...")
-    clear_screen()
+    
+    # Bar loading akan muncul saat menunggu
+    for _ in tqdm(range(10), desc="Loading", bar_format="{l_bar}{bar} {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}{postfix}]"):
+        time.sleep(0.5) 
 
 def race():
     distance = 50
     player_position = 1
     computer_position = 1
-    
+
     while player_position < distance and computer_position < distance:
         clear_screen()
-        print("go go go!")
+        print("Go go go!")
         print("Distance:", "-" * distance)
         print("Player:", " " * (player_position - 1) + "P")
-        print("computer:", " " * (computer_position - 1) + "C")
-        
+        print("Computer:", " " * (computer_position - 1) + "C")
+
         time.sleep(0.2)
-        
+
         player_move = random.randint(1, 3)
         computer_move = random.randint(1, 3)
-        
+
         player_position += player_move
         computer_position += computer_move
-    
+
     clear_screen()
     if player_position >= distance:
-        print("you win!")
+        print("You win!")
     else:
-        print("you lose!")
+        print("You lose!")
 
 def main():
     display_intro()
